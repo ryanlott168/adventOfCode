2c747984c503d4a36792245e1a672f6e1a7d5a18
Ryan Lott <ryanclott@outlook.com>
2021-12-13 10:26:09 -0600

    Completed Day 3 - Part 2

--- day_3/binaryDiagnostic.js
+++ day_3/binaryDiagnostic.js
@@ -12,9 +12,9 @@ const binaryToDecimalConverter = binaryNumber => {
     return aggregate;
 }
 
+// Part 1.
 
 const findPowerConsumption = data => {
-    let powerConsumption = 0;
     let gammaRateBinary = '';
     let epsilonRateBinary = '';
 
@@ -35,8 +35,74 @@ const findPowerConsumption = data => {
         }
     }
 
-    powerConsumption = binaryToDecimalConverter(gammaRateBinary) * binaryToDecimalConverter(epsilonRateBinary);
+    let powerConsumption = binaryToDecimalConverter(gammaRateBinary) * binaryToDecimalConverter(epsilonRateBinary);
     return powerConsumption;
 }
 
 // findPowerConsumption(diagnosticReportData);
+
+
+// Part 2.
+
+const getO2GeneratorRating = data => {
+    let potentialRatings = data;
+
+        for(let i = 0; potentialRatings.length > 1 && i < potentialRatings[0].length; i++) {
+            const storage = {0: [], 1: []};
+            for(let j = 0; j < potentialRatings.length; j++) {
+                if(potentialRatings[j][i] === '0') {
+                    storage[0].push(potentialRatings[j]);
+                } else {
+                    storage[1].push(potentialRatings[j]);
+                }
+            }
+
+            if (storage[0].length <= storage[1].length) {
+                potentialRatings = storage[1];
+            } else {
+                potentialRatings = storage[0];
+            }
+        }
+
+    let o2GeneratorRating = potentialRatings.pop();
+    o2GeneratorRating = binaryToDecimalConverter(o2GeneratorRating);
+
+    return o2GeneratorRating;
+}
+
+
+const getCO2ScrubberRating = data => {
+    let potentialRatings = data;
+
+        for(let i = 0; potentialRatings.length > 1 && i < potentialRatings[0].length; i++) {
+            const storage = {0: [], 1: []};
+            for(let j = 0; j < potentialRatings.length; j++) {
+                if(potentialRatings[j][i] === '0') {
+                    storage[0].push(potentialRatings[j]);
+                } else {
+                    storage[1].push(potentialRatings[j]);
+                }
+            }
+
+            if (storage[0].length <= storage[1].length) {
+                potentialRatings = storage[0];
+            } else {
+                potentialRatings = storage[1];
+            }
+        }
+
+    let co2ScrubberRating = potentialRatings.pop();
+    co2ScrubberRating = binaryToDecimalConverter(co2ScrubberRating);
+
+    return co2ScrubberRating;
+}
+
+const findLifeSupportRating = data => {
+    const o2GeneratorRating = getO2GeneratorRating(data);
+    const co2ScrubberRating = getCO2ScrubberRating(data);
+    
+    const lifeSupportRating = o2GeneratorRating * co2ScrubberRating;
+    return lifeSupportRating
+}
+
+// findLifeSupportRating(diagnosticReportData);
