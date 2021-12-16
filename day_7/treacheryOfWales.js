0dd88d38110dda758747d7632a3a5a6b55801a67
Ryan Lott <ryanclott@outlook.com>
2021-12-16 15:47:01 -0600

    Day 7 Complete

---
+++ day_7/treacheryOfWales.js
@@ -0,0 +1,62 @@
+import dataParser from '../helper/dataParser.js';
+
+const crabPositionData = await dataParser('./data.txt', ',', data => data.map(position => parseInt(position)));
+
+const sampleData = [16,1,2,0,4,2,7,1,2,14]
+let range = crabPositionData.reduce((prev, curr) => {
+    if(prev[0] > curr) prev[0] = curr;
+    if(prev[1] < curr) prev[1] = curr;
+    return prev;
+}, [crabPositionData[0], crabPositionData[0]]);
+
+
+// Part 1. Linear Fuel Cost
+const findBestPositionAndFuelCost = (range, crabPositionData) => {
+    let bestPosition;
+    let lowestCost;
+
+    for(let i = range[0]; i <= range[1]; i++) {
+        const cost = crabPositionData.reduce((prev, curr) => {
+            return prev + Math.abs(i - curr)
+        }, 0);
+
+        if(!bestPosition || cost < lowestCost) {
+            lowestCost = cost;
+            bestPosition = i;
+        }
+    }
+    return {bestPosition, lowestCost};
+}
+
+
+// Part 2. Exponential Fuel Cost
+const findBestPositionAndFuelCost2 = (range, crabPositionData) => {
+    let bestPosition;
+    let lowestCost;
+
+    for(let i = range[0]; i <= range[1]; i++) {
+        const cost = crabPositionData.reduce((prev, curr) => {
+            let change = Math.abs(i - curr);
+            if(change < 1) return prev;
+
+            let individualCost = 0;
+            for(let i = 1; i <= change; i++) {
+                individualCost += i;
+            }
+            return prev + individualCost;
+
+        }, 0);
+
+        if(!bestPosition || cost < lowestCost) {
+            lowestCost = cost;
+            bestPosition = i;
+        }
+    }
+    return {bestPosition, lowestCost};
+}
+
+
+// const linearGrowthBestPositionAndCost = findBestPositionAndFuelCost(range, crabPositionData);
+// const exponentialGrowthBestPositionAndCost = findBestPositionAndFuelCost2(range, crabPositionData);
+// console.log(linearGrowthBestPositionAndCost);
+// console.log(exponentialGrowthBestPositionAndCost);
